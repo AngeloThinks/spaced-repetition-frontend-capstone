2b70eafd654f02177a7056cb381a766de9451bde
Angelo Vazquez <angelovazquez@Angelos-MacBook-Pro.local>
2021-01-22 03:18:37 -0600

    vercel

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 
 export default {
-  API_ENDPOINT: 'https://stormy-forest-60463.herokuapp.com/api',
+  API_ENDPOINT: 'https://peaceful-thicket-76380.herokuapp.com/api',
   // API_ENDPOINT : 'http://localhost:8000/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 };
